de4ea8555743dbed8690293754b78660d4cfeb98
kittiphot01 <97584437+kittiphot01@users.noreply.github.com>
2022-02-01 21:16:03 +0700

    Create page1.php

---
+++ page1.php
@@ -0,0 +1,20 @@
+<html>
+  <head> 
+    <title>page 1</title> 
+  </head>
+  <body bgcolor="lightblue">
+    <h1 style="color:darkblue;">ประวัติส่วนตัว</h1>
+    <br> 
+    <b>ชื่อ :</b> <i>กิตติพศ พั้วพวง</i>
+    <br> 
+    <b>ชื่อเล่น :</b> <i>ขิง</i> 
+    <br>
+    <b>ชั้น :</b> <i>ม.4/12</i>
+    <br> 
+    <b>เลขที่ :</b> <i>3</i>
+    <br>
+    <b>เลขประจำตัวนักเรียน :</b> <i>57012</i>
+    <br>
+    <b>โรงเรียน :</b> <i>หล่มสักวิทยาคม<i> 
+    </body>
+    </html>
