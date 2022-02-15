54b384402d5220a16119e95c07e7535024fdadf9
kittiphot01 <97584437+kittiphot01@users.noreply.github.com>
2022-02-15 10:26:54 +0700

    Update mypage1.php

--- mypage1.php
+++ mypage1.php
@@ -3,7 +3,7 @@
     <title>งานเรื่อง link</title>
   </head>
   <body>
-    <a href="www.facebook.com/kittiphot.puapuang.9<h1>จิ้มจิ้ม</h1></a>
+    <a href="www.facebook.com/kittiphot.puapuang.9"<h1>จิ้มจิ้ม</h1></a>
     <a href="mypage2.php"><h1>page 2</h1></a>
   </body>
 </html>
