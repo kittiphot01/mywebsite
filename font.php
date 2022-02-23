7cc2ee31363ebaf91fa7093efc90278bf3629c66
kittiphot01 <97584437+kittiphot01@users.noreply.github.com>
2022-02-23 14:32:01 +0700

    Update font.php

--- font.php
+++ font.php
@@ -4,14 +4,22 @@
     <style>
       @font-face{
         font-family: "MyFont1";
-        src: url("MiPancakeBold.ttf");
+        src: url("TA16BIT-Regular.ttf");
       }
-      body{
+      @font-face{
+        font-family: "MyFont2";
+        src: url("sd chokdee.ttf");
+      }
+      #font1{
         font-family: "MyFont1";
+      }
+      #font2{
+        font-family: "MyFont2";
       }
     </style>
   </head>
   <body>
-    <h1>ฟอนต์ของฉัน</h1>
+    <h1 id="font1">วรรณวิสา นิตยพันธ์ ม.4/12 เลขที่ 24 เลขประจำตัว 57070</h1>
+    <h1 id="font2">วรรณวิสา นิตยพันธ์ ม.4/12 เลขที่ 24 เลขประจำตัว 57070</h1>
   </body>
 </html>
