7e3127c674e694608495f504701482db202af400
kittiphot01 <97584437+kittiphot01@users.noreply.github.com>
2022-03-02 13:22:57 +0700

    Create table.php

---
+++ table.php
@@ -0,0 +1,36 @@
+<html>
+  <head>
+    <title>การสร้างตาราง</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ</th>
+        <th>รูปภาพ</th>
+        <th>Twitter/FB</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>ยิ่งคุณ รอดทิม</td>
+        <td><img src="https://i.pinimg.com/280x280_RS/94/e4/d1/94e4d1683b63958c29e2fa96a176f016.jpg" width="45px"></td>
+        <td><a href="https://www.facebook.com/yunking.rodtim">Facebook</a></td>
+         <tr>
+        <td>1</td>
+        <td>ยิ่งคุณ รอดทิม</td>
+        <td><img src="https://i.pinimg.com/280x280_RS/94/e4/d1/94e4d1683b63958c29e2fa96a176f016.jpg" width="45px"></td>
+        <td><a href="https://www.facebook.com/yunking.rodtim">Facebook</a></td>
+            <tr>
+        <td>1</td>
+        <td>ยิ่งคุณ รอดทิม</td>
+        <td><img src="https://i.pinimg.com/280x280_RS/94/e4/d1/94e4d1683b63958c29e2fa96a176f016.jpg" width="45px"></td>
+        <td><a href="https://www.facebook.com/yunking.rodtim">Facebook</a></td>
+               <tr>
+        <td>1</td>
+        <td>ยิ่งคุณ รอดทิม</td>
+        <td><img src="https://i.pinimg.com/280x280_RS/94/e4/d1/94e4d1683b63958c29e2fa96a176f016.jpg" width="45px"></td>
+        <td><a href="https://www.facebook.com/yunking.rodtim">Facebook</a></td>
+      </tr>
+    </table>
+  </body>
+</html>
