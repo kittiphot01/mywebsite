004919733135400877357a508f891964010f2dd5
kittiphot01 <97584437+kittiphot01@users.noreply.github.com>
2022-03-02 13:29:00 +0700

    Update table.php

--- table.php
+++ table.php
@@ -12,9 +12,9 @@
       </tr>
       <tr>
         <td>1</td>
-        <td>ยิ่งคุณ รอดทิม</td>
-        <td><img src="https://i.pinimg.com/280x280_RS/94/e4/d1/94e4d1683b63958c29e2fa96a176f016.jpg" width="45px"></td>
-        <td><a href="https://www.facebook.com/yunking.rodtim">Facebook</a></td>
+        <td>Fs Superbusss</td>
+        <td><img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fliquipedia.net%2Fvalorant%2FSuperBusS&psig=AOvVaw0NK7j6knj8yufEpObjdIFL&ust=1646288826143000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPjknPnlpvYCFQAAAAAdAAAAABAD" width="45px"></td>
+        <td><a href="https://www.facebook.com/Superbusss-102353164511263/">Facebook</a></td>
          <tr>
         <td>1</td>
         <td>ยิ่งคุณ รอดทิม</td>
