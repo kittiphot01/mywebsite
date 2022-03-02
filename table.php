d921013acff9ff2a397a86f7f29c45202270d0b2
kittiphot01 <97584437+kittiphot01@users.noreply.github.com>
2022-03-02 13:30:33 +0700

    Update table.php

--- table.php
+++ table.php
@@ -13,7 +13,7 @@
       <tr>
         <td>1</td>
         <td>Fs Superbusss</td>
-        <td><img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fliquipedia.net%2Fvalorant%2FSuperBusS&psig=AOvVaw0NK7j6knj8yufEpObjdIFL&ust=1646288826143000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPjknPnlpvYCFQAAAAAdAAAAABAD" width="45px"></td>
+        <td><img src="https://fullsense.gg/wp-content/uploads/2022/01/Superbus.png" width="45px"></td>
         <td><a href="https://www.facebook.com/Superbusss-102353164511263/">Facebook</a></td>
          <tr>
         <td>1</td>
