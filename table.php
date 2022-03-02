66b587fb2091d1ff65d469829ca208716dcd1117
kittiphot01 <97584437+kittiphot01@users.noreply.github.com>
2022-03-02 13:39:15 +0700

    Update table.php

--- table.php
+++ table.php
@@ -8,28 +8,34 @@
         <th>ลำดับ</th>
         <th>ชื่อ</th>
         <th>รูปภาพ</th>
-        <th>Twitter/FB</th>
+        <th>Instagram/FB</th>
       </tr>
       <tr>
         <td>1</td>
-        <td>Fs Superbusss</td>
+        <td>FS SUPERBUSS</td>
         <td><img src="https://fullsense.gg/wp-content/uploads/2022/01/Superbus.png" width="45px"></td>
         <td><a href="https://www.facebook.com/Superbusss-102353164511263/">Facebook</a></td>
          <tr>
         <td>1</td>
-        <td>ยิ่งคุณ รอดทิม</td>
-        <td><img src="https://i.pinimg.com/280x280_RS/94/e4/d1/94e4d1683b63958c29e2fa96a176f016.jpg" width="45px"></td>
-        <td><a href="https://www.facebook.com/yunking.rodtim">Facebook</a></td>
+        <td>FS JOHNOLSEN</td>
+        <td><img src="https://fullsense.gg/wp-content/uploads/2022/01/Johnolsen.png" width="45px"></td>
+        <td><a href="https://www.facebook.com/JohnOlsen-805616289787266/">Facebook</a></td>
             <tr>
         <td>1</td>
-        <td>ยิ่งคุณ รอดทิม</td>
-        <td><img src="https://i.pinimg.com/280x280_RS/94/e4/d1/94e4d1683b63958c29e2fa96a176f016.jpg" width="45px"></td>
-        <td><a href="https://www.facebook.com/yunking.rodtim">Facebook</a></td>
+        <td>FS PTC</td>
+        <td><img src="https://fullsense.gg/wp-content/uploads/2022/01/PTC.png" width="45px"></td>
+        <td><a href="https://www.facebook.com/kititkawinptc">Facebook</a></td>
                <tr>
         <td>1</td>
-        <td>ยิ่งคุณ รอดทิม</td>
-        <td><img src="https://i.pinimg.com/280x280_RS/94/e4/d1/94e4d1683b63958c29e2fa96a176f016.jpg" width="45px"></td>
-        <td><a href="https://www.facebook.com/yunking.rodtim">Facebook</a></td>
+        <td>FS LAMMYSNAX</td>
+        <td><img src="https://fullsense.gg/wp-content/uploads/2022/01/Lammy.png" width="45px"></td>
+        <td><a href="https://www.facebook.com/LammyVAL">Facebook</a></td>
+                  <tr>
+        <td>1</td>
+        <td>FS CHALALALA</td>
+        <td><img src="https://fullsense.gg/wp-content/uploads/2022/01/Chlalala.png" width="45px"></td>
+        <td><a href="https://www.instagram.com/g_chanitpak/">Instagram</a></td>
+                     <tr>
       </tr>
     </table>
   </body>
