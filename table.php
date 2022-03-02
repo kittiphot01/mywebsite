acb9f6f5bb88ed66b1c322cce196b6648deb226b
kittiphot01 <97584437+kittiphot01@users.noreply.github.com>
2022-03-02 13:40:25 +0700

    Update table.php

--- table.php
+++ table.php
@@ -16,22 +16,22 @@
         <td><img src="https://fullsense.gg/wp-content/uploads/2022/01/Superbus.png" width="45px"></td>
         <td><a href="https://www.facebook.com/Superbusss-102353164511263/">Facebook</a></td>
          <tr>
-        <td>1</td>
+        <td>2</td>
         <td>FS JOHNOLSEN</td>
         <td><img src="https://fullsense.gg/wp-content/uploads/2022/01/Johnolsen.png" width="45px"></td>
         <td><a href="https://www.facebook.com/JohnOlsen-805616289787266/">Facebook</a></td>
             <tr>
-        <td>1</td>
+        <td>3</td>
         <td>FS PTC</td>
         <td><img src="https://fullsense.gg/wp-content/uploads/2022/01/PTC.png" width="45px"></td>
         <td><a href="https://www.facebook.com/kititkawinptc">Facebook</a></td>
                <tr>
-        <td>1</td>
+        <td>4</td>
         <td>FS LAMMYSNAX</td>
         <td><img src="https://fullsense.gg/wp-content/uploads/2022/01/Lammy.png" width="45px"></td>
         <td><a href="https://www.facebook.com/LammyVAL">Facebook</a></td>
                   <tr>
-        <td>1</td>
+        <td>5</td>
         <td>FS CHALALALA</td>
         <td><img src="https://fullsense.gg/wp-content/uploads/2022/01/Chlalala.png" width="45px"></td>
         <td><a href="https://www.instagram.com/g_chanitpak/">Instagram</a></td>
