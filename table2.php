dc010db9ac2ba415bb7ef94fc183442fda433958
kittiphot01 <97584437+kittiphot01@users.noreply.github.com>
2022-03-05 02:33:14 +0700

    Create table2.php

---
+++ table2.php
@@ -0,0 +1,42 @@
+<html>
+  <head>
+    <title>การสร้างตาราง</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ</th>
+        <th>รูปภาพ</th>
+        <th>instagram/twitte</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>Krit threemandown</td>
+        <td><img src="https://i.mydramalist.com/2j8r7_5f.jpg" width="45px"></td>
+        <td><a href="https://www.instagram.com/kittyumbs/">instagram</a></td>
+         <tr>
+        <td>2</td>
+        <td>ดิว จิรวรรตน์</td>
+        <td><img src="https://pbs.twimg.com/profile_images/1376866175712288773/RdXGp7Qr_400x400.jpg" width="45px"></td>
+        <td><a href="https://mobile.twitter.com/dew_jsu">twitte</a></td>
+            <tr>
+        <td>3</td>
+        <td>Yorch yongsin</td>
+        <td><img src="https://i.pinimg.com/originals/30/83/9e/30839eb4a32d0c3140f8fbdded5c7f8a.jpg" width="45px"></td>
+        <td><a href="https://www.instagram.com/yorch_yongsin/?utm_medium=copy_link">instagram</a></td>
+               <tr>
+        <td>4</td>
+        <td>LUCAS</td>
+        <td><img src="https://thethaiger.com/th/wp-content/uploads/sites/9/2021/08/lucas_xx444_119428584_1000259773769604_3909642076302582139_n-1-820x600.jpg" width="45px"></td>
+        <td><a href="https://www.instagram.com/lucas_xx444/?utm_medium=copy_link">instagram</a></td>
+                  <tr>
+        <td>5</td>
+        <td>บิลลี่ ณัฐดนัย ชูชาติ</td>
+        <td><img src="https://sgp1.digitaloceanspaces.com/adaybulletin/2018/03/feature_hesaid_528_Billy-1.jpg" width="45px"></td>
+        <td><a href="https://www.instagram.com/billbilly01/?utm_medium=copy_link">Instagram</a></td>
+                     <tr>
+      </tr>
+    </table>
+  </body>
+</html>
